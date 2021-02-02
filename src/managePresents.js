5e2e3eb5606bac4976cf29131db2babf0f8d8722
jonathanx111 <jonathanx111@gmail.com>
2021-02-01 21:04:03 -0500

    final

--- src/managePresents.js
+++ src/managePresents.js
@@ -1,3 +1,9 @@
-export function managePresents(state, action) {
+export function managePresents(state = {numberOfPresents: 0}, action) {
   // your code here
+  switch (action.type) {
+    case "presents/increase":
+      return {numberOfPresents: state.numberOfPresents + 1}
+    default: 
+      return state
+  }
 }
